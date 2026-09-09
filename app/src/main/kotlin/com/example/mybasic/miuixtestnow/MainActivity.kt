4de951add55d500b5953390c684c3088ffa68809
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-09 14:49:34 +0800

    更新 MainActivity.kt

--- app/src/main/kotlin/com/example/mybasic/miuixtestnow/MainActivity.kt
+++ app/src/main/kotlin/com/example/mybasic/miuixtestnow/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.example.mybasic.miuixtestnow
 
+import App
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.mybasic.miuixtestnow.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
-class MainActivity : AppCompatActivity() {
-    
-    private var _binding: ActivityMainBinding? = null
-    private val binding: ActivityMainBinding
-        get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        
-        binding.textView.text = "Hello, Basic Activity!"
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+
+        setContent {
+            var colorMode by remember { mutableIntStateOf(0) }
+            val darkMode = when (colorMode) {
+                2, 5 -> true
+                0, 3 -> isSystemInDarkTheme()
+                else -> false
+            }
+
+            DisposableEffect(darkMode) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT) { darkMode },
+                    navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT) { darkMode },
+                )
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    window.isNavigationBarContrastEnforced = false // Xiaomi moment, this code must be here
+                }
+
+                onDispose {}
+            }
+
+            App(onColorModeChange = { colorMode = it })
+        }
     }
 }
