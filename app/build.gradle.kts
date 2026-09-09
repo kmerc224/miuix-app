d80c782c371ab5f6b87666ba41e0c991d8f87739
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-09 14:12:09 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("top.yukonga.miuix.kmp:miuix-ui:<version>")
     implementation(libs.androidx.constraintlayout)
 }
