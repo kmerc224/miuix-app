00045fa3debfa48cd8ed59c2e22e981381acb160
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-10 20:28:43 +0800

    更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,18 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+dependencyResolution {
     repositories {
         google()
         mavenCentral()
