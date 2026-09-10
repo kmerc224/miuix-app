29f49c04f92838734f122d68b1f13a53ab3b5d32
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-10 20:44:39 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.mybasic.miuixtestnow"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.mybasic.miuixtestnow"
