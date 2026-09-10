11b0062cbbbf94d8f89961e010011a6f8f92590d
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-10 21:29:55 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mybasic.miuixtestnow"
-        minSdk = 26
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
