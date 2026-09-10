789b5a064acdda95f60ae7ed5b1e651c7441e977
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-11 02:46:35 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-// Top-level build file
 plugins {
-    id("com.android.application") version "9.4.0" apply false
-    // 删除：id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
