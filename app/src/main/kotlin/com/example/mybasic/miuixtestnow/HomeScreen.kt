aa717545f470e448d323702c19a68cb2ec7d98df
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-11 17:46:32 +0800

    Update HomeScreen.kt

--- app/src/main/kotlin/com/example/mybasic/miuixtestnow/HomeScreen.kt
+++ app/src/main/kotlin/com/example/mybasic/miuixtestnow/HomeScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.unit.dp
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.icon.MiuixIcons
-// 基础图标从 icons 包导入
+// 基础图标：从 icon.icons 包导入（miuix-ui 内置）
 import top.yukonga.miuix.kmp.icon.icons.ArrowRight
 import top.yukonga.miuix.kmp.icon.icons.Check
 import top.yukonga.miuix.kmp.theme.MiuixTheme
