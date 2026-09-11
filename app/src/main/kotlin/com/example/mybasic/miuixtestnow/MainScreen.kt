2178f02680b731cf2630bb284ac126dc757d28f3
acckmerc <158418671+kmerc224@users.noreply.github.com>
2026-09-11 17:20:18 +0800

    更新 MainScreen.kt

--- app/src/main/kotlin/com/example/mybasic/miuixtestnow/MainScreen.kt
+++ app/src/main/kotlin/com/example/mybasic/miuixtestnow/MainScreen.kt
@@ -10,9 +10,6 @@ import top.yukonga.miuix.kmp.basic.NavigationBarItem
 import top.yukonga.miuix.kmp.basic.Scaffold
 import top.yukonga.miuix.kmp.basic.TopAppBar
 import top.yukonga.miuix.kmp.icon.MiuixIcons
-import top.yukonga.miuix.kmp.icon.icons.VerticalSplit
-import top.yukonga.miuix.kmp.icon.icons.Contacts
-import top.yukonga.miuix.kmp.icon.icons.Settings
 
 @Composable
 fun MainScreen(
